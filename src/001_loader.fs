e24f60fa96801f7a392bbbd840e96c61301a7a00
Marcos Cruz <github@programandala.net>
2017-12-11 19:51:28 +0100

    Update library&code to Solo Forth 0.14.0-pre.367

--- src/001_loader.fs
+++ src/001_loader.fs
@@ -5,17 +5,12 @@
 
 ( Black Flag -- load block  )
 
-[defined] need
-?\ 2 load
+2 load
   \ Compile the `need` utility, which must be in block 2.
-
-  \ need decode need words need order need dump
-  \ XXX TMP -- for debugging
 
 need load-program
 
 load-program black-flag
-
   \ Compile the game.
 
   \ vim: filetype=soloforth
